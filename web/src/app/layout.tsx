3d000f25c4700e9f23ed364b649a802b53e01ea2
xiaguoxuan <xia.guoxuan@gmail.com>
2026-03-29 20:57:22 +0800

    Update layout.tsx

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+}); 
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
