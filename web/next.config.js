c9e6287c184782a38fdc05c4770723730ccc5943
xiaguoxuan <xia.guoxuan@gmail.com>
2026-03-29 21:03:34 +0800

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   output: 'export', // 必须开启
+  images: {
+    unoptimized: true, // 静态导出必须开启这个
+  },
+  
   // 核心：允许公网 IP 访问开发资源 (Next.js 15/16)
   allowedDevOrigins: ['43.156.161.212', '43.156.161.212:3001'],
   
